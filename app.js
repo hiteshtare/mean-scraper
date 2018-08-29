f5540e79aac8957a8efb48422c12084fd171d208
Hitesh Tare <hiteshtare@gmail.com>
2018-08-29 17:08:15 +0530

    #Changes: Port No added.

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const config = require('./config');
 const webScraperModule = require('./webScraper');
 
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function () {
+  console.log("Listening on Port 3000");
+});
+
 //Connect to Database
 mongoose.connect(config.getDbConnStr(), {
   useNewUrlParser: true
